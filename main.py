ed1fd5e3c634e685584609dae37d0f8892cb739c
Gourav <gouravgunaga555@gmail.com>
2021-06-27 12:04:34 +0530

    Programe to fetch the data from the vaccine api is complete

--- main.py
+++ main.py
@@ -5,7 +5,29 @@ def data_fetcher(pincode, date):
     url = f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pincode}&date={date}'
     print(url)  # remove this
     data = requests.get(url)
-    print(data)
+    if data.status_code == 200:  # which means connection is established
+        center = data.json()[
+            'centers']  # loaded the ceneters to the center variable
+        if (center):  # TO check centers are available or not if no send else
+            for num in center:
+                for session in num['sessions']:
+                    if (session['available_capacity_dose1']
+                            or session['available_capacity_dose2']):
+                        msg = '\nDATE: ' + session['date'] + '\nHOSTIPAL NAME: ' + num[
+                            'name'] + '\nADDRESS: ' + num[
+                                'address'] + '\nAGE LIMIT: ' + str(
+                                    session['min_age_limit']
+                                ) + '\nPRICE: ' + num[
+                                    'fee_type'] + '\nNAME OF THE VACCINE: ' + session[
+                                        'vaccine'] + '\nDOSE 1 COUNTS: ' + str(
+                                            session['available_capacity_dose1']
+                                        ) + '\nDOSE 2 COUNTS: ' + str(
+                                            session['available_capacity_dose2']
+                                        )
+                        print(msg)
+
+        else:
+            print('RIGHT NOW NO CENTERS ARE AVAILABLE TO SHOW')
 
 
 pincode = 574201
